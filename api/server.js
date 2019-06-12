7836ceb55583d6318b1e585565532c5c500ee3bd
James Wigdor <jpwigdor@gmail.com>
2019-06-11 22:49:35 -0400

    Added Cookies and Sessions

--- api/server.js
+++ api/server.js
@@ -1,12 +1,35 @@
+// Imports
 const express = require("express");
 const helmet = require("helmet");
-const server = express();
+const session = require("express-session");
+const SessionStore = require("connect-session-knex")(session);
 
-//routes
+// Routes
 const userRoutes = require("../users/userRoutes.js");
 const registerRoutes = require("../users/registerRoutes.js");
 const loginRoutes = require("../users/loginRoutes.js");
 
+const server = express();
+const sessionConfig = {
+  name: "monkey",
+  secret: "super secret string",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 60 * 60 * 1000, // 1 hour. 60sec + 60 min + 1000 miliseconds
+    secure: false,
+    httpOnly: true
+  },
+  store: new SessionStore({
+    knex: require("../data/dbConfig.js"),
+    tablename: "sessions",
+    sidfieldname: "sid", // which column to store the session ID, SID.
+    createtable: true, // creates table if it doesn't exist through our migrations.
+    clearInterval: 60 * 60 * 1000 // time in session before it expires. // 60 minutes, 60 seconds, 1000 miliseconds
+  })
+};
+
+server.use(session(sessionConfig));
 server.use(helmet());
 server.use(express.json());
 
